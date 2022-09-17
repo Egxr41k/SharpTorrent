0df0d7bdd7e2618589de78403c95f13e0a041791
Egxr41k <egor2005krava@gmail.com>
2022-09-18 00:10:34 +0300

    finish writing console client, its work

--- ConsoleTorrent/Program.cs
+++ ConsoleTorrent/Program.cs
@@ -1,6 +1,5 @@
 ﻿using MonoTorrent;
 using MonoTorrent.Client;
-using System;
 using System.Net;
 using System.Text;
 
@@ -11,14 +10,16 @@ class Program
     static void Main(string[] args)
     {
         var Listener = new Top10Listener(10);
-        string? TorrentPath;
+        string? TorrentPath, DownloadPath;
         do
         {
-            Console.Write("Enter th torrent path: ");
+            Console.Write("Enter the torrent file path: ");
             TorrentPath = Console.ReadLine();
+            Console.Write("Enter the download folder path: ");
+            DownloadPath = Console.ReadLine();
         }
-        while (!File.Exists(TorrentPath));
-        var downloadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"\Downloads");
+        while (!File.Exists(TorrentPath) &&
+            !Directory.Exists(DownloadPath));
 
         Task.Run(async () =>
         {
@@ -33,62 +34,57 @@ class Program
                 HttpStreamingPrefix = $"http://127.0.0.1:{55125}/"
             }.ToSettings());
 
-
             try
             {
                 var settingsBuilder = new TorrentSettingsBuilder();
-                var torrent = await Torrent.LoadAsync(TorrentPath ?? "#");
-                var manager = await Engine.AddAsync(
-                    torrent,
-                    downloadPath,
-                    settingsBuilder.ToSettings());
+                TorrentManager? manager = await Engine.AddAsync(
+                    await Torrent.LoadAsync(
+                        TorrentPath ?? "#"),
+                        DownloadPath ?? "#",
+                        settingsBuilder.ToSettings());
 
                 manager.PeersFound += delegate (object? sender, PeersAddedEventArgs e)
                 {
                     lock (Listener)
                         Listener.WriteLine($"Found {e.NewPeers} new peers and {e.ExistingPeers} existing peers");
                 };
-
-                Console.WriteLine(manager.InfoHashes.V1OrV2.ToHex());
             }
-            catch (Exception e)
-            {
-                Console.Write("Couldn't decode {0}: ", TorrentPath);
-                Console.WriteLine(e.Message);
-            }
+            catch (Exception e) { Console.WriteLine("Couldn't decode file. " + e.Message); }
 
-            foreach (TorrentManager manager in Engine.Torrents)
-            {
-                manager.PeerConnected += (o, e) => {
-                    lock (Listener)
-                        Listener.WriteLine($"Connection succeeded: {e.Peer.Uri}");
-                };
-                manager.ConnectionAttemptFailed += (o, e) => {
-                    lock (Listener)
-                        Listener.WriteLine(
-                            $"Connection failed: {e.Peer.ConnectionUri} - {e.Reason}");
-                };
 
-                manager.PieceHashed += delegate (object? o, PieceHashedEventArgs e) {
-                    lock (Listener)
-                        Listener.WriteLine($"Piece Hashed: {e.PieceIndex} - {(e.HashPassed ? "Pass" : "Fail")}");
-                };
+            var torrent = Engine.Torrents[0];
 
-                manager.TorrentStateChanged += delegate (object? o, TorrentStateChangedEventArgs e) {
-                    lock (Listener)
-                        Listener.WriteLine($"OldState: {e.OldState} NewState: {e.NewState}");
-                };
+            
+            torrent.PeerConnected += (o, e) => {
+                lock (Listener)
+                    Listener.WriteLine($"Connection succeeded: {e.Peer.Uri}");
+            };
+            torrent.ConnectionAttemptFailed += (o, e) => {
+                lock (Listener)
+                    Listener.WriteLine(
+                        $"Connection failed: {e.Peer.ConnectionUri} - {e.Reason}");
+            };
 
-                manager.TrackerManager.AnnounceComplete += (sender, e) => {
-                    Listener.WriteLine($"{e.Successful}: {e.Tracker}");
-                };
+            torrent.PieceHashed += delegate (object? o, PieceHashedEventArgs e) {
+                lock (Listener)
+                    Listener.WriteLine($"Piece Hashed: {e.PieceIndex} - {(e.HashPassed ? "Pass" : "Fail")}");
+            };
 
-                await manager.StartAsync();
-            }
+            torrent.TorrentStateChanged += delegate (object? o, TorrentStateChangedEventArgs e) {
+                lock (Listener)
+                    Listener.WriteLine($"OldState: {e.OldState} NewState: {e.NewState}");
+            };
 
-            StringBuilder sb = new StringBuilder(1024);
+            torrent.TrackerManager.AnnounceComplete += (sender, e) => {
+                Listener.WriteLine($"{e.Successful}: {e.Tracker}");
+            };
 
-            while (Engine.IsRunning)
+            await torrent.StartAsync();
+            
+
+            StringBuilder sb = new(1024);
+
+            do
             {
                 sb.Remove(0, sb.Length);
 
@@ -106,56 +102,54 @@ class Program
                 foreach (var mapping in Engine.PortMappings.Pending)
                     sb.AppendLine($"Pending mapping:      {mapping.PublicPort}:{mapping.PrivatePort} ({mapping.Protocol})");
 
-                foreach (TorrentManager manager in Engine.Torrents)
+                sb.AppendLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+                sb.AppendLine($"State:              {torrent.State}");
+                sb.AppendLine($"Name:               {(torrent.Torrent == null ? "MetaDataMode" : torrent.Torrent.Name)}");
+                sb.AppendLine($"Progress:           {torrent.Progress:0.00}");
+                sb.AppendLine($"Transferred:        {torrent.Monitor.DataBytesReceived / 1024.0 / 1024.0:0.00} MB ↓ / {torrent.Monitor.DataBytesSent / 1024.0 / 1024.0:0.00} MB ↑");
+                sb.AppendLine($"Tracker Status");
+                foreach (var tier in torrent.TrackerManager.Tiers)
+                    sb.AppendLine($"\t{tier.ActiveTracker} : Announce Succeeded: {tier.LastAnnounceSucceeded}. Scrape Succeeded: {tier.LastScrapeSucceeded}.");
+
+                if (torrent.PieceManager != null)
+                    sb.AppendFormat("Current Requests:   {0}", await torrent.PieceManager.CurrentRequestCountAsync());
+
+                var peers = await torrent.GetPeersAsync();
+
+                sb.AppendLine("Outgoing:");
+                foreach (PeerId p in peers.Where(t => t.ConnectionDirection == Direction.Outgoing))
                 {
-                    sb.AppendLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-                    sb.AppendLine($"State:              {manager.State}");
-                    sb.AppendLine($"Name:               {(manager.Torrent == null ? "MetaDataMode" : manager.Torrent.Name)}");
-                    sb.AppendLine($"Progress:           {manager.Progress:0.00}");
-                    sb.AppendLine($"Transferred:        {manager.Monitor.DataBytesReceived / 1024.0 / 1024.0:0.00} MB ↓ / {manager.Monitor.DataBytesSent / 1024.0 / 1024.0:0.00} MB ↑");
-                    sb.AppendLine($"Tracker Status");
-                    foreach (var tier in manager.TrackerManager.Tiers)
-                        sb.AppendLine($"\t{tier.ActiveTracker} : Announce Succeeded: {tier.LastAnnounceSucceeded}. Scrape Succeeded: {tier.LastScrapeSucceeded}.");
-
-                    if (manager.PieceManager != null)
-                        sb.AppendFormat("Current Requests:   {0}", await manager.PieceManager.CurrentRequestCountAsync());
-
-                    var peers = await manager.GetPeersAsync();
-
-                    sb.AppendLine("Outgoing:");
-                    foreach (PeerId p in peers.Where(t => t.ConnectionDirection == Direction.Outgoing))
-                    {
-                        sb.AppendFormat("\t{2} - {1:0.00}/{3:0.00}kB/sec - {0} - {4} ({5})", p.Uri,
-                                                                                    p.Monitor.DownloadRate / 1024.0,
-                                                                                    p.AmRequestingPiecesCount,
-                                                                                    p.Monitor.UploadRate / 1024.0,
-                                                                                    p.EncryptionType,
-                                                                                    string.Join("|", p.SupportedEncryptionTypes.Select(t => t.ToString()).ToArray()));
-                    }
-                    sb.AppendLine();
-                    sb.AppendLine("Incoming:");
-                    foreach (PeerId p in peers.Where(t => t.ConnectionDirection == Direction.Incoming))
-                    {
-                        sb.AppendFormat("\t{2} - {1:0.00}/{3:0.00}kB/sec - {0} - {4} ({5})", p.Uri,
-                                                                                    p.Monitor.DownloadRate / 1024.0,
-                                                                                    p.AmRequestingPiecesCount,
-                                                                                    p.Monitor.UploadRate / 1024.0,
-                                                                                    p.EncryptionType,
-                                                                                    string.Join("|", p.SupportedEncryptionTypes.Select(t => t.ToString()).ToArray()));
-                    }
-
-                    sb.AppendLine();
-                    if (manager.Torrent != null)
-                        foreach (var file in manager.Files)
-                            sb.AppendFormat("{1:0.00}% - {0}", file.Path, file.BitField.PercentComplete);
+                    sb.AppendFormat("\t{2} - {1:0.00}/{3:0.00}kB/sec - {0} - {4} ({5})", p.Uri,
+                                                                                p.Monitor.DownloadRate / 1024.0,
+                                                                                p.AmRequestingPiecesCount,
+                                                                                p.Monitor.UploadRate / 1024.0,
+                                                                                p.EncryptionType,
+                                                                                string.Join("|", p.SupportedEncryptionTypes.Select(t => t.ToString()).ToArray()));
                 }
-                Console.Clear ();
-                Console.WriteLine (sb.ToString ());
-                Listener.ExportTo (Console.Out);
+                sb.AppendLine();
+                sb.AppendLine("Incoming:");
+                foreach (PeerId p in peers.Where(t => t.ConnectionDirection == Direction.Incoming))
+                {
+                    sb.AppendFormat("\t{2} - {1:0.00}/{3:0.00}kB/sec - {0} - {4} ({5})", p.Uri,
+                                                                                p.Monitor.DownloadRate / 1024.0,
+                                                                                p.AmRequestingPiecesCount,
+                                                                                p.Monitor.UploadRate / 1024.0,
+                                                                                p.EncryptionType,
+                                                                                string.Join("|", p.SupportedEncryptionTypes.Select(t => t.ToString()).ToArray()));
+                }
 
-                await Task.Delay(500);
+                sb.AppendLine();
+                if (torrent.Torrent != null)
+                    foreach (var file in torrent.Files)
+                        sb.AppendFormat("{1:0.00}% - {0}", file.Path, file.BitField.PercentComplete);
+
+                Console.Clear();
+                Console.WriteLine(sb.ToString());
+                Listener.ExportTo(Console.Out);
+
             }
-        }).Wait();
+            while (torrent.Files[0].BitField.PercentComplete != 100.00);
 
+        }).Wait();
     }
 }
